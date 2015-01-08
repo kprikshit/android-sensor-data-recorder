747be49ffe8db10e02888c8764aff2a7588d0c02
unknown <kprikshit22@gmail.com>
2015-01-08 14:33:16 +0530

    New ABOUT with recycler view

--- app/src/main/java/com/example/prikshit/recorder/About.java
+++ app/src/main/java/com/example/prikshit/recorder/About.java
@@ -3,23 +3,61 @@ package com.example.prikshit.recorder;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class About extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        //setting the toolbar
         Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
         //for showing back buttons to go back on previous activity
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //setting thr recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.recycler1);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MyAdapter(getData());
+        recyclerView.setAdapter(adapter);
     }
 
+    //Data for the recycler view list
+    public List<Information> getData(){
+        List<Information> data = new ArrayList<>();
+        int[] pics = {R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic,R.drawable.prikshit_pic};
+        String[] names={"Prikshit","Pankaj","Parmeet","Prikshit","Pankaj","Parmeet","Prikshit","Pankaj","Parmeet","Prikshit","Pankaj","Parmeet"};
+        String[] emails={"kprikshit22","as","ss","kprikshit22","as","ss","kprikshit22","as","ss","kprikshit22","as","ss"};
+        for(int i=0;i<pics.length;i++){
+            Information info = new Information();
+            info.picId=pics[i];
+            info.email=emails[i];
+            info.name=names[i];
+            data.add(info);
+        }
+        return data;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/prikshit/recorder/Information.java
@@ -0,0 +1,12 @@
+package com.example.prikshit.recorder;
+
+/**
+ * Data Model for our GUI.
+ * Created by Prikshit on 08-01-2015.
+ */
+public class Information {
+    //Information for list objects
+    String name;
+    String email;
+    int picId;
+}
